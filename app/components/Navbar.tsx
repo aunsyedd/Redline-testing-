0b3de57ead9a8d35c7b4414b0b5a105aca93c08a
Syed Aun <auns5757@gmail.com>
2026-05-22 19:59:28 +0500

    price deck added

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
   const links = [
     { label: "View Our Cinematic Shoots", href: "/shoots" },
     { label: "Explore The Plans", href: "/Plans" },
-    { label: "Pricing Deck", href: "/Pkg2" },
+    { label: "Plan's Pricing Deck", href: "/Pkg2" },
     { label: "Contact", href: "/contact" },
   ];
 
