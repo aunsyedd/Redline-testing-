74407bbf01ae074f014f66c07fa1a406e2b41a0d
Syed Aun <auns5757@gmail.com>
2026-05-30 16:25:40 +0500

    favicon

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.redlinevfx.com',
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 5000,
+}
